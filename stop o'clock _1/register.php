a4f873b21c7b2c64faa39e506f3b579958018eea
Blio <bryan2opiyo@gmail.com>
2014-09-24 16:08:55 +0300

    C

---
+++ stop o'clock _1/register.php
@@ -0,0 +1,97 @@
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Register to Play Stop</title>
+        <link rel="stylesheet" type="text/css" href="styles/styles.css"/>
+        <link href="styles/bootstrap.min.css" rel="stylesheet"/>
+        <script src="js/bootstrap.min.js"></script>
+        
+        <style>
+		body {
+			background-color:#FFF;
+			font-size:30px;	
+	}
+				
+h1 {
+	color:#F33;
+	alignment:center;
+	font-family:"Comic Sans MS", cursive;
+	
+	}
+		
+		
+		
+		 </style>
+        
+</head>
+
+<body>
+
+<div id="content">
+<p align="center"><strong>Register to Play STOP</strong></p>
+
+
+<form id="insertform" name="insertform" method="post" action="insert.php">
+  <table width="619" border="1" align="center">
+    <tr>
+      <td colspan="5"><span class="style1">Persons Details </span></td>
+    </tr>
+    <tr>
+      <td width="92">Firstname : </td>
+      <td width="161"><label>
+        <input name="fname" type="text" id="fname" />
+      </label></td>
+      <td width="110">Lastname: </td>
+      <td width="151"><label>
+        <input name="lname" type="text" id="lname" />
+      </label></td>
+      <td width="71">&nbsp;</td>
+      
+      
+      
+    </tr>
+    <tr>
+      <td><div align="right">Username:</div></td>
+      <td><label>
+        <input name="username" type="text" id="username" />
+      </label></td>
+      <td>Password: </td>
+      <td><label>
+        <input name="password" type="password" id="password" />
+      </label></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td>Email:</td>
+      <td><label>
+        <input name="emailaddress" type="text" id="emailaddress" />
+      </label></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td><label>
+        <input type="submit" class="btn btn-success" name="Submit" value="Add Record" />
+      </label></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+  </table>
+</form>
+<br/>
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+</body>
+</html>
+
+</div>
+
+</body>
+</html>
